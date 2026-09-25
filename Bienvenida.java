ac6daa308a389fa44729895903b8b004384feddb
Santiago Nuñez <stgnnzp@ciencias.unam.mx>
2026-09-25 11:44:03 -0600

    Cambio hola rick sanchez

--- Bienvenida.java
+++ Bienvenida.java
@@ -1,5 +1,5 @@
 public class Bienvenida {
 	public static void main(String[] args) {
-		System.out.println("Santiago Azael Nuñez Calleja \n"+ 324178896 +"\nApex Legends" );
+		System.out.println("Hola Rick Sanchez" );
 	}
 }
